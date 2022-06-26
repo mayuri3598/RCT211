79532bab989dc4e5031457aea6232eb7787fb941
mayuri3598 <97494267+mayuri3598@users.noreply.github.com>
2022-06-26 13:22:43 +0530

    redux todo

---
+++ React-Redux/Redux-api's/redux-api/src/components/login.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import { AuthContext } from "./authcontext"
+import { useNavigate } from "react-router-dom";
+let Login = ()=>{
+    let {auth} = useContext(AuthContext)
+
+    let navigate = useNavigate()
+    return (
+        <div><input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <button onClick={()=>{
+         auth.status = true
+         navigate(-2 , { replace: true })
+        }}>Login</button>
+        </div>
+    )
+}
+
+export default Login
